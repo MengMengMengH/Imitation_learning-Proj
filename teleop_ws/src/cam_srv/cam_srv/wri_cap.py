9325f810de137acc556a53b5252218bb14abfcd5
hanmeng <hanmg@buaa.edu.cn>
2025-10-27 10:14:50 +0800

    add time_stamp in camera node

--- teleop_ws/src/cam_srv/cam_srv/wri_cap.py
+++ teleop_ws/src/cam_srv/cam_srv/wri_cap.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray,Header
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 import cv2
-import queue
 import asyncio
 import signal
 from typing import Any, Callable
@@ -23,13 +22,19 @@ class Wri_Cam_Cap(Node):
         self.get_logger().info("Wrist camera node started")
         self.cam_pub_ = self.create_publisher(Image, "wrist_camera_Image", 10)
         self.bridge_ = CvBridge()
-        self.get_logger().info(f"{cv2.__file__}")
-        self.target_fps = 30
+        # self.get_logger().info(f"{cv2.__file__}")
+        self.target_fps = 10
+        self.timer_period = 1.0 / self.target_fps
+        self.timer_ = self.create_timer(self.timer_period, self.timer_callback)
         
-        self.q: queue.Queue[Any] = queue.Queue()
+        self.current_frame = None
+        self.frame_lock = threading.Lock()
         self.running = True  
 
         self.cap = self.opencv_init()
+        if not self.cap.isOpened():
+            rclpy.shutdown()
+            return
 
         self.read_thread = threading.Thread(target=self.read_frame, daemon=True)
         self.display_thread = threading.Thread(target=self.display, daemon=True)
@@ -38,10 +43,9 @@ class Wri_Cam_Cap(Node):
 
 
     def opencv_init(self):
-        cap = cv2.VideoCapture("/dev/video0",cv2.CAP_FFMPEG)
-        if not cap:
+        cap = cv2.VideoCapture("/dev/video0",cv2.CAP_V4L2)
+        if not cap.isOpened():
             self.get_logger().error("Error: Could not open video.")
-            rclpy.shutdown()
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         return cap
@@ -49,51 +53,50 @@ class Wri_Cam_Cap(Node):
     def read_frame(self):
         while rclpy.ok() and self.running:
             try:
-                ret,self.frame = self.cap.read()
+                ret,frame = self.cap.read()
                 if ret is None:
                     continue
-                if not self.q.empty():
-                    self.q.get_nowait()
-                self.q.put(self.frame)
-            except queue.Empty:
-                pass
+                with self.frame_lock:
+                    self.current_frame = frame
             except Exception as e:
                 self.get_logger().error(f"Error: {e}")
 
     def display(self):
         cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("frame", 800, 600)
-        while rclpy.ok() and self.running:
+        while rclpy.ok() and self.running:            
             try:
-                frame = self.q.get()
-                if frame is not None:
-                    cv2.imshow("frame", frame)
+                frame_to_display = None
+                with self.frame_lock:
+                    frame_to_display = self.current_frame
+                if frame_to_display is not None:
+                    cv2.imshow("frame", frame_to_display)
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         self.running = False
                         break
-            except queue.Empty:
-                pass
             except Exception as e:
                 self.get_logger().error(f"Error: {e}")
         cv2.destroyAllWindows()
 
     def timer_callback(self):
         try:
-            pass
-            if self.frame is not None:
-                self.frame_ = self.q.get()
-                image_msg = self.bridge_.cv2_to_imgmsg(self.frame_, encoding="bgr8")
+            frame_to_publish = None
+            with self.frame_lock:
+                frame_to_publish = self.current_frame.copy() if self.current_frame is not None else None
+            if frame_to_publish is not None:
+                msg_Header = Header()
+                msg_Header.stamp = self.get_clock().now().to_msg()
+
+                image_msg = self.bridge_.cv2_to_imgmsg(frame_to_publish, encoding="bgr8",header=msg_Header)
                 self.cam_pub_.publish(image_msg)
-        except queue.Empty:
-            self.get_logger().error("Queue is empty")
-            pass
+
         except Exception as e:
             self.get_logger().error(f"Error: {e}")
 
     def destroy_node(self):
         self.get_logger().info("Shutting down camera node...")
         self.running = False  
-        time.sleep(1) 
+        time.sleep(1)
         self.cap.release()
         cv2.destroyAllWindows()
         super().destroy_node()
@@ -106,12 +109,10 @@ def main(args=None):
         wri_cam_cap.get_logger().info("Received SIGINT, shutting down...")
         wri_cam_cap.destroy_node()
         rclpy.shutdown()
-
     signal.signal(signal.SIGINT, handler)
     try:
         rclpy.spin(wri_cam_cap)
     except KeyboardInterrupt:
         pass 
     finally:
-        wri_cam_cap.destroy_node()
-        rclpy.shutdown()
+        pass
